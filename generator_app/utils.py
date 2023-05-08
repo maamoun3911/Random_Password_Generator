464330bfd605c8e765abc7c62bc42727d9674484
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-05-08 05:19:45 +0300

    8/5/2023

--- generator_app/utils.py
+++ generator_app/utils.py
@@ -8,6 +8,6 @@ def pass_gen(length: int, type: str = ""):
     strange_group = "_-@&%$#!\/."
     groups = [digits, ascii_lowercase, ascii_uppercase, strange_group]
     for i in range(length-len(type)):
-        group = groups[randint(0, 3)]
+        group = groups[randint(0, len(groups))]
         password += group[randint(0, len(group)-1)]
     return password[:len(password)//2]+type+password[len(password)//2:]
