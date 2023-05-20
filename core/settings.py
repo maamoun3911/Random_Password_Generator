2360a58512a7a5c285c362a47097f52d2c87fe14
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-05-20 06:56:23 +0300

    20/5/2023 third time

--- core/settings.py
+++ core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*qjp_em!o@bw6nja$brb7$z6ue*&)*i9bum=$hilzl(u3id&=m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
